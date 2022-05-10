0a486112184aa09ad068d24783ec131134f981e6
Gustavo Scarpellini <scarpellini1992@gmail.com>
2022-05-10 15:42:18 -0300

    implementando cards e animações

---
+++ src/components/Experiencias/ExperienciaItem.tsx
@@ -0,0 +1,17 @@
+import { ItemContainer } from './styles';
+
+export default function ExperienciaItem() {
+  return (
+    <ItemContainer>
+      <div>
+        <h1>2021</h1>
+        <h2>Dev Front-end</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+          excepturi, quaerat eum corrupti repudiandae at laudantium veniam ut
+          dolores.
+        </p>
+      </div>
+    </ItemContainer>
+  );
+}
---
+++ src/components/Experiencias/index.tsx
@@ -0,0 +1,20 @@
+import SectionTitle from '../SectionTitle';
+import ExperienciaItem from './ExperienciaItem';
+import { Container } from './styles';
+
+function Experiencias() {
+  return (
+    <Container>
+      <SectionTitle title="#05 Anos" description="de Experiência" />
+
+      <section>
+        <ExperienciaItem />
+        <ExperienciaItem />
+        <ExperienciaItem />
+        <ExperienciaItem />
+      </section>
+    </Container>
+  );
+}
+
+export default Experiencias;
