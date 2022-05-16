9320df3bdbe6867ea87b1558741596b2aa512041
Gustavo Scarpellini <scarpellini1992@gmail.com>
2022-05-16 16:15:52 -0300

    implementando estado no formulario

--- src/components/FormContato/Form.tsx
+++ src/components/FormContato/Form.tsx
@@ -1,11 +1,36 @@
+import { FormEvent, useState } from 'react';
 import { FormContainer, Input, TextArea } from './styles';
 
 export default function Form() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log(nome, email, mensagem);
+  }
   return (
-    <FormContainer data-aos="fade-up">
-      <Input placeholder="Nome" required />
-      <Input placeholder="Email" type="email" required />
-      <TextArea placeholder="Mensagem" required />
+    <FormContainer data-aos="fade-up" onSubmit={handleSubmit}>
+      <Input
+        placeholder="Nome"
+        value={nome}
+        onChange={({ target }) => setNome(target.value)}
+        required
+      />
+      <Input
+        placeholder="Email"
+        type="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+        required
+      />
+      <TextArea
+        placeholder="Mensagem"
+        value={mensagem}
+        onChange={({ target }) => setMensagem(target.value)}
+        required
+      />
       <button type="submit">ENVIAR</button>
     </FormContainer>
   );
